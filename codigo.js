899b37e3aff0ca080a0de6369f126ab31cad8809
Samuel Linares <samu.f167@gmail.com>
2023-08-07 20:04:01 -0300

    primer comentario

--- codigo.js
+++ codigo.js
@@ -1,48 +1,41 @@
 // Objeto de libro
 function libro(titulo, autor, genero) {
-    this.titulo = titulo;
-    this.autor = autor;
-    this.genero = genero;
-  }
-  
-  // Array para guardar los libros de la wishlist
-  let wishlist = [];
-  
-  // Función para mostrar los libros agregados a mi wishlist
-  function mostrarwishlist() {
-    console.log("Esta es mi wishlist");
-    wishlist.forEach((libro, muestra) => {console.log(`${muestra + 1}. Titulo: ${libro.titulo}, Autor: ${libro.autor}, Genero: ${libro.genero}`);});
-  }
-  
-  // Función para agregar un nuevo libro a mi wishlist
-  function agregarlibro() {
-    const titulolibro = prompt("Ingresa el titulo del libro:");
-    const autorlibro = prompt("Ingresa el autor del libro:");
-    const generolibro = prompt("Ingresa el genero del libro:");
-  
-    wishlist.push(new libro(titulolibro, autorlibro, generolibro));
-    console.log("Agregaste tu libro a la wishlist");
-  }
-  
+  this.titulo = titulo;
+  this.autor = autor;
+  this.genero = genero;
+}
 
-  // do while para hacer el prompt que preguntara si se agrega un libro o se finaliza y que pedira los datos del libro que se quiere agregar
- let opcion;
-  do {
-    mostrarwishlist();
-  
-    opcion = prompt("Escribi el número indicado para cada opción: 1 para agregar un nuevo libro a la wishlist y 2 para finalizar");
-  
-    switch (opcion) {
-      case "1":
-        agregarlibro();
-        break;
-  
-      case "2":
-        console.log("Se termino de agregar libros");
-        break;
-  
-      default:
-        console.log("Esta opción NO es correcta. Las opciónes correctas son el número 1 o 2");
-    }
-  } while (opcion !== "2");
-  
+// Array para guardar los libros de la wishlist
+let wishlist = [];
+
+// mostrar los libros agregados a mi wishlist con DOM como una lista
+function mostrarwishlist() {
+  const wishlistelement = document.getElementById("wishlist");
+  wishlistelement.innerHTML = "";
+
+  wishlist.forEach((libro, mostrar) => {
+      const li = document.createElement("li");
+      li.textContent = `${mostrar + 1}. titulo: ${libro.titulo}, autor: ${libro.autor}, genero: ${libro.genero}`;
+      wishlistelement.appendChild(li);
+  });
+}
+
+// agregar un nuevo libro a mi wishlist con DOM
+function agregarlibro() {
+  const titulolibro = document.getElementById("titulo").value;
+  const autorlibro = document.getElementById("autor").value;
+  const generolibro = document.getElementById("genero").value;
+
+  wishlist.push(new libro(titulolibro, autorlibro, generolibro));
+  mostrarwishlist();
+}
+
+// botón para agregar libros
+const agregarlibrobutton = document.getElementById("agregarlibro");
+agregarlibrobutton.addEventListener("click", agregarlibro);
+
+// Mensaje en popup avisando que se se termino de agregar los libros
+const finalizarbutton = document.getElementById("finalizar");
+finalizarbutton.addEventListener("click", () => {
+  alert("Terminaste de agregar tus libros");
+});
